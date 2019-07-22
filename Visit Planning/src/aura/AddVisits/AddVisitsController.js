fe6b581d13cbb2cec202a00de6be4de2fc28f275
Sachin- PC <Sachin- PC@DESKTOP-IJJ3P4V>
2019-07-22 20:09:45 +0530

    Commit : Test Classes 22-07-2019

--- Visit Planning/src/aura/AddVisits/AddVisitsController.js
+++ Visit Planning/src/aura/AddVisits/AddVisitsController.js
@@ -72,5 +72,24 @@
     },
     clearData : function(component, event, helper) {
         $A.get('e.force:refreshView').fire();
+    },
+    // method set previous date based on selected input date
+    onClickPreviousDateButton :function (component, event,helper) {
+        var currentDate = new Date(component.get('v.selectedDate'));
+        var previousDay = new Date(currentDate);
+        previousDay.setDate(currentDate.getDate() - 1);
+        var setpreviousDate = $A.localizationService.formatDate(previousDay, "YYYY-MM-DD");
+        component.set('v.selectedDate',setpreviousDate);
+        
+        
+    },
+    // method set Next date based on selected input date
+    onClickNextDateButton :function (component, event,helper) {
+        var currentDate = new Date(component.get('v.selectedDate'));
+        var nextDay = new Date(currentDate);
+        nextDay.setDate(currentDate.getDate() + 1);
+        var setNextDate = $A.localizationService.formatDate(nextDay, "YYYY-MM-DD");
+        component.set('v.selectedDate',setNextDate);
+      
     }
 })
